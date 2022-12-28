155a4ce109cb6a7271f0bbbc3490924ae97c68ed
roopabs23 <111435160+roopabs23@users.noreply.github.com>
2022-12-28 15:19:13 +0530

    Add files via upload

---
+++ pooja.py
@@ -0,0 +1,92 @@
+Last login: Wed Dec 14 11:21:14 on ttys000
+roopashree.bs@K00G4K6W7K ~ % mkdir -p project_name
+roopashree.bs@K00G4K6W7K ~ % cd project_name
+
+roopashree.bs@K00G4K6W7K project_name % git init
+
+Reinitialized existing Git repository in /Users/roopashree.bs/project_name/.git/
+roopashree.bs@K00G4K6W7K project_name % cat test_notebook.py
+cat: test_notebook.py: No such file or directory
+roopashree.bs@K00G4K6W7K project_name % touch pooja.py
+roopashree.bs@K00G4K6W7K project_name % nano pooja.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                                                           File: pooja.py                                                           Modified  
+
+import random
+print(random.randint(0, 5) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                  [ Unknown Command: ^S ]                                                                   
+^G Get Help               ^O WriteOut               ^R Read File              ^Y Prev Pg                ^K Cut Text               ^C Cur Pos                
+^X Exit                   ^J Justify                ^W Where is               ^V Next Pg                ^U UnCut Text             ^T To Spell              
